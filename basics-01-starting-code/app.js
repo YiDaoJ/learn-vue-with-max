2ae8181734d7ae7463f041fd2d2f551f06a320ef
YiDaoJ <jcxsusan@gmail.com>
2022-09-11 16:35:48 +0200

    create instance and interporlate

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,11 @@
+// 1. create vue instance
+const app = Vue.createApp({
+  data(){
+    return {
+      courseGoal: 'Finish the course and learn vue!'
+    }
+  }
+})
+
+// 2. connect vue instance to html
+app.mount('#user-goal') // unique identifier
