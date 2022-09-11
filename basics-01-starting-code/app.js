f19c847bed8e0953e91295c9ccd09ca2abf27eea
YiDaoJ <jcxsusan@gmail.com>
2022-09-11 17:23:39 +0200

    declare and call vue method

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -5,7 +5,13 @@ const app = Vue.createApp({
       courseGoal: "Finish the course and learn vue!",
       vueLink: "https://vuejs.org/"
     }
-  }
+  },
+  methods: {
+    chooseGoal() {
+      const randomNumber = Math.random()
+      return randomNumber < 0.5 ? "Master Vue!" : "Learn Vue!"
+    }
+  },
 })
 
 // 2. connect vue instance to html
